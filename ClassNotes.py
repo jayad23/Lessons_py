4328d932248d086d4ad26158e30f28a37fb96e35
jayad23 <jaidervanegas@live.com>
2022-04-05 04:48:40 +0200

    April 5th

---
+++ ClassNotes.py
@@ -0,0 +1,24 @@
+"""""
+variables in lowercase are var / let
+
+name = "kike"
+
+Variable in UPPERCASE are constants
+
+NAME = "Kike"
+
+Variables with Capitalization is used for names of class.
+Name = "Kike" 
+
+There are three different types of numbers in Python. 
+
+Integers,
+Float,
+Complex. Which takes a real integer, and an imaginary one
+E.g.
+complex_1 = complex(2, 3)
+
+We can use the method type() to understand the type of the variable data.
+
+When we use triple (") the string value of a varible can break off in different lines.
+"""""
