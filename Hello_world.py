c052417f098eefe253bdc5ba15c5a9381da46bc2
jayad23 <jaidervanegas@live.com>
2022-04-05 03:29:49 +0200

    first commit

---
+++ Hello_world.py
@@ -0,0 +1,12 @@
+"""""
+variables in lowercase are var / let
+
+name = "kike"
+
+Variable in UPPERCASE are constants
+
+NAME = "Kike"
+
+Variables with Capitalization is used for names of class.
+Name = "Kike" 
+"""""
